b6664ac556ace7ce78f38a0a8286b748a7d94958
siddu0603 <132552273+siddu0603@users.noreply.github.com>
2025-04-07 22:25:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -69,5 +69,5 @@ def predict():
     return jsonify({'prediction': class_name, 'confidence': float(confidence)})
 
 # Run Flask app
-if __name__ == '__main__':
-    app.run(debug=True)
+ if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
